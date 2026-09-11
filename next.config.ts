235a66771d660a8ffa675a5a9d87d2eb05b2dca6
Faisal Fajri <faisalfjrii@gmail.com>
2026-09-11 14:57:06 +0700

    chore: add Docker configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: process.env.VERCEL ? undefined : "standalone",
   reactCompiler: true,
 };
 
